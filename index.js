d432d0241a09f499b2164b34ca1d0e805683cbd8
mrsahadatarefin <sahadatarifin806@gmail.com>
2022-11-24 22:50:54 +0600

    hello

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT ||5000;
 
+const app = express()
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w5yg5ut.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.send('car resale market server is running')
 
